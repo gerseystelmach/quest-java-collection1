d290175537e375ab392aa851c316da83b163cb1f
Gersey Stelmach <gerseystelmach@gmail.com>
2021-09-27 14:04:21 +0200

    solution for quete collection 1

--- Thanos.java
+++ Thanos.java
@@ -5,8 +5,39 @@ public class Thanos {
     public static void main(String[] args) {
 
         // TODO 1 : Create an empty heroes list
+        ArrayList<Hero> heros = new ArrayList<>();
 
         // TODO 2 : Add those heroes to the list
+        Hero blackWidow = new Hero("Black Widow", 34);
+        Hero captainAmerica = new Hero("Captain America", 100);
+        Hero vision = new Hero ("Vision", 3);
+        Hero ironMan = new Hero ("Iron Man", 48);
+        Hero scarletWitch = new Hero ("Scarlet Witch", 29);
+        Hero thor = new Hero ("Thor", 1500);
+        Hero hulk = new Hero ("Hulk", 49);
+        Hero doctorStrange = new Hero ("Doctor Strange", 42);
+
+        heros.add(blackWidow);
+        heros.add(captainAmerica);
+        heros.add(vision);
+        heros.add(ironMan);
+        heros.add(scarletWitch);
+        heros.add(thor);
+        heros.add(hulk);
+        heros.add(doctorStrange);
+
+        /*
+        heros.add(new Hero ("Black Widow", 34));
+        heros.add(new Hero("Captain America", 100));
+        heros.add(new Hero ("Vision", 3));
+        heros.add(new Hero ("Iron Man", 48));
+        heros.add(new Hero ("Scarlet Witch", 29));
+        heros.add(new Hero ("Thor", 1500));
+        heros.add(new Hero ("Hulk", 49));
+        heros.add(new Hero ("Doctor Strange", 42));
+*/
+
+
         // name: Black Widow, age: 34
         // name: Captain America, age: 100
         // name: Vision, age: 3
@@ -18,10 +49,18 @@ public class Thanos {
 
         // TODO 3 : It's Thor birthday, now he's 1501
 
+        thor.setAge(1501);
+
+
         // TODO 4 : Shuffle the heroes list
 
-        // TODO 5 : Keep only the half of the list
+        Collections.shuffle(heros);
 
+        // TODO 5 : Keep only the half of the list
+        List<Hero> herosHalfList = heros.subList((heros.size()/2), heros.size());
         // TODO 6 : Loop throught the list and display the name of the remaining heroes
+        for (Hero hero : herosHalfList) {
+            System.out.println(hero.getName());
+        }
     }
 }
